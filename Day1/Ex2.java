eeb8bbd58f9a3b8dacf1d558c2c9a272bd45a5d2
VasanthRamSJ <125632231+VasanthRamSJ@users.noreply.github.com>
2023-02-21 10:52:47 +0530

    rectified a mistake

--- Day1/Ex2.java
+++ Day1/Ex2.java
@@ -6,15 +6,20 @@ public class Ex2 {
 	   static float val3;
 	   static int val4;
 	   static long val5;
-	   static String val6;
+	   static char val6;
+	   static byte val7;
+	   static short val8;
 	   public static void main(String[] args) {
 	      System.out.println("Default values.....");
-	      System.out.println("Val1 = " + val1);
-	      System.out.println("Val2 = " + val2);
-	      System.out.println("Val3 = " + val3);
-	      System.out.println("Val4 = " + val4);
-	      System.out.println("Val5 = " + val5);
-	      System.out.println("Val6 = " + val6);
+	      System.out.println("Double value = " + val2);
+	      System.out.println("Float value = " + val3);
+	      System.out.println("Integer value = " + val4);
+	      System.out.println("Long value = " + val5);
+	      System.out.println("Char value = " + val6);
+	      System.out.println("Byte value = " + val7);
+	      System.out.println("Short value = " + val8);
+	      System.out.println("Boolean value = " + val1);
+
 	   }
 }
  
