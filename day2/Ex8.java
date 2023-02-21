de708bf6372ea9c3deef8e4339223d419f4384aa
VasanthRamSJ <125632231+VasanthRamSJ@users.noreply.github.com>
2023-02-21 13:03:35 +0530

    self rectified mistake 3

--- day2/Ex8.java
+++ day2/Ex8.java
@@ -1,14 +1,28 @@
 package day2;
 
 public class Ex8 {
-
+	 public static void main(String args[])
+	    {
+	        Singleton x = Singleton.getInstance();
+	        Singleton y = Singleton.getInstance();  
+	        System.out.println("Hashcode of x is " + x.hashCode());
+	        System.out.println("Hashcode of y is " + y.hashCode());
+	        if (x == y) {
+	            System.out.println(
+	                "two object references point to the same memory location on the heap i.e, to the same object");
+	        }
+	        else {
+	            System.out.println(
+	                "two objects references DO NOT point to the same memory location on the heap");
+	        }
+	    }
 }
 class Singleton {
     private static Singleton single_instance = null;
     public String s;
     private Singleton()
     {
-        s = "Hello I am a string part of Singleton class";
+        System.out.println("Hello I am a string part of Singleton class");
     }
     public static Singleton getInstance()
     {
@@ -16,24 +30,5 @@ class Singleton {
             single_instance = new Singleton();
   
         return single_instance;
-    }
-}
-class GFG {
-    public static void main(String args[])
-    {
-        Singleton x = Singleton.getInstance();
-        Singleton y = Singleton.getInstance();  
-        System.out.println("Hashcode of x is "
-                           + x.hashCode());
-        System.out.println("Hashcode of y is "
-                           + y.hashCode());
-        if (x == y) {
-            System.out.println(
-                "Three objects point to the same memory location on the heap i.e, to the same object");
-        }
-        else {
-            System.out.println(
-                "Three objects DO NOT point to the same memory location on the heap");
-        }
     }
 }
