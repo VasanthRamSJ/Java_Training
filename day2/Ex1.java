3ad384d127e22c8e08777a811da47c93332bb69f
VasanthRamSJ <125632231+VasanthRamSJ@users.noreply.github.com>
2023-02-21 11:16:29 +0530

    self rectified mistake 2

--- day2/Ex1.java
+++ day2/Ex1.java
@@ -1,27 +1,35 @@
 package day2;
-
+import java.util.*;
 public class Ex1 {
-	public static void main(String[] args) {
-	    int [] arr = new int[2];
-	    arr[0] = 4;
-	    arr[1] = 5;
+	int x;
+    public static void main(String[] args) {
+        int n = 9;
+        double d = 16.0d;
+        Ex1 e = new Ex1();
+        int []a = new int[] {0,0,0,0,0};
+        System.out.println("before function call : "+n+", "+d);
+        primitive(n, d);
+        System.out.println(" after function call : "+n+", "+d);
+        System.out.println("Array values before function call:"+Arrays.toString(a));
+        array(a);
+        System.out.println("Array values after function call:"+Arrays.toString(a));
+        System.out.println("Value of class member x before function call : "+e.x);
+        object(e);
+        System.out.println("Value of class member x after function call : "+e.x);
+    }
 
-	    changeContent(arr);
+    public static void primitive(int n, double d) {
+        n =n+1;
+        d =d-2.5f;
+    }
 
-	    System.out.println(arr[0]);  
-	  
-	    changeRef(arr);
+    public static void array(int []a) {
+        for(int i=0; i<a.length; i++)
+            a[i]=a[i]+1;
+    }
 
-	    System.out.println(arr[0]);  
-	}
-	public static void changeContent(int[] arr) {
-
-		   arr[0] = 10;  
-		}
-
-		public static void changeRef(int[] arr) {
-		  
-		   arr = new int[2];  
-		   arr[0] = 15;
-		}
+    public static void object(Ex1 d) {
+        d.x = 190;
+    }
 }
+
