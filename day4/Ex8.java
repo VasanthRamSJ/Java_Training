c591ccb64526f1b26e26fee55f4707f7c2001761
VasanthRamSJ <125632231+VasanthRamSJ@users.noreply.github.com>
2023-02-23 10:40:31 +0530

    self rectified mistake 1

--- day4/Ex8.java
+++ day4/Ex8.java
@@ -26,9 +26,31 @@ public static void main(String args[]) throws Exception{
 	System.out.println("Enter subject 5 marks : ");
 	String subject_5=input.nextLine();
 	int sub5 = Integer.parseInt(subject_5);
+	
+	System.out.println("Enter subject 6 marks : ");
+	String subject_6=input.nextLine();
+	int sub6 = Integer.parseInt(subject_6);
+	
+	System.out.println("Enter subject 7 marks : ");
+	String subject_7=input.nextLine();
+	int sub7 = Integer.parseInt(subject_7);
+	
+	System.out.println("Enter subject 8 marks : ");
+	String subject_8=input.nextLine();
+	int sub8 = Integer.parseInt(subject_8);
+	
+	System.out.println("Enter subject 9 marks : ");
+	String subject_9=input.nextLine();
+	int sub9 = Integer.parseInt(subject_9);
+	
+	System.out.println("Enter subject 10 marks : ");
+	String subject_10=input.nextLine();
+	int sub10 = Integer.parseInt(subject_10);
+	
+	
 
 	
-	avg=(sub1+sub2+sub3+sub4+sub5)/5;
+	avg=(sub1+sub2+sub3+sub4+sub5+sub6+sub7+sub8+sub9+sub10)/10;
 System.out.println("Average : "+avg);
 		
 
